22cfba365780b27d5a8749256bffa850bd6adbde
Jyrki Muukkonen <jvtm@kruu.org>
2018-03-15 20:15:10 +0200

    flow: initial plugin class, action loading

--- puro/api.py
+++ puro/api.py
@@ -13,8 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import asyncio
 import logging
 import uuid
+from copy import deepcopy
 
 from .plugins import BasePlugin, Registry
 
@@ -44,11 +46,53 @@ class Flow:
         "puro.plugins.jmespath.JMESPath",
     )
 
-    def __init__(self):
+    def __init__(self, config):
+        self.config = config
         self.log = logging.getLogger(self.__class__.__name__)
+
+        # Load plugin classes
         self.registry = Registry()
-        for mod_path in self.DEFAULT_PLUGINS:
+        self.load_plugins([{"class": x} for x in self.DEFAULT_PLUGINS], ignore_import_errors=True)
+        self.load_plugins(config["plugins"])
+
+        # Load actions, service plugins etc (instances of plugin classes)
+        self.actions = self.load_instances(config["actions"])
+        self.service_plugins = self.load_instances(config["service_plugins"])
+
+        # TODO: load rules
+        # self.root = Node()
+
+        # TODO: assign readers/producers
+
+        self.queue = asyncio.Queue(maxsize=config["queue_size"])
+        self.running = None
+
+    def load_plugins(self, plugins, ignore_import_errors=False):
+        catch = ModuleNotFoundError if ignore_import_errors else ()
+        for plugin in plugins:
+            name = plugin.get("name")
+            mod_path = plugin.get("class")
             try:
-                self.registry.load_class(mod_path, base_class=BasePlugin)
-            except ImportError as ex:
-                self.log.info("Failed to load plugin class %r: %s", mod_path, ex)
+                self.registry.load_class(mod_path, name=name, base_class=BasePlugin)
+            except catch as ex:
+                self.log.info("Failed to load plugin class %r %s", plugin, ex)
+
+    def load_instances(self, items):
+        loaded = {}
+        for item in items:
+            name = item["name"]
+            kwargs = deepcopy(item)
+            plugin_name = kwargs.pop("plugin")
+            loaded[name] = self.registry.get_instance(plugin_name, **kwargs)
+            self.log.info("Loaded instance %s %s %r", name, plugin_name, loaded[name])
+        return loaded
+
+    def create_task(self, obj) -> Task:
+        # TODO: item context, stats, ...
+        return Task(obj, service_context=self.service_plugins)
+
+    async def enque(self, item):
+        if not isinstance(item, Task):
+            item = self.create_task(item)
+        await self.queue.put(item)
+        return item
--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,12 @@ from puro.api import Flow
 
 
 def test_load_defaults():
-    # Nothing real yet...
-    flow = Flow()
+    # Nothing real yet... waiting for config helper cleanups
+    config = {
+        "plugins": [],
+        "actions": [],
+        "service_plugins": [],      # FIXME: better name
+        "queue_size": 10,
+    }
+    flow = Flow(config)
     assert flow.registry
